f058071c4bb083df5a802e804003713bab3ef353
ClaraCamejo <clara.camejo@hotmail.com>
2021-05-21 20:07:00 -0300

    bd y model de solicitud GTM-23

---
+++ Model/SolicitudesModel.php
@@ -0,0 +1,19 @@
+<?php
+
+class solicitudesModel{
+
+    private $db;
+
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=Cooperativa;charset=utf8','root','');
+    }
+
+
+    function agregarAutor($nombre, $direccion, $telefono, $franja_horaria, $volumen_materiales){//tengo que tereminar este
+        
+        $sentencia=$this->db->prepare('INSERT INTO solicitud_pedido (nombre_apellido, direccion, telefono, franja_horaria, volumen_materiales) VALUES (?,?,?,?,?)');
+        $sentencia->execute([$nombre, $direccion, $telefono, $franja_horaria, $volumen_materiales]);
+        
+        return $this->db->lastInsertId();
+    }
+}
---
+++ base de datos/cooperativa.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-05-2021 a las 00:53:41
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cooperativa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitud_pedido`
+--
+
+CREATE TABLE `solicitud_pedido` (
+  `nsolicitud` bigint(20) UNSIGNED NOT NULL,
+  `nombre_apellido` varchar(150) NOT NULL,
+  `direccion` varchar(150) NOT NULL,
+  `telefono` varchar(150) NOT NULL,
+  `franja_horaria` varchar(150) NOT NULL,
+  `volumen_materiales` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `solicitud_pedido`
+--
+ALTER TABLE `solicitud_pedido`
+  ADD PRIMARY KEY (`nsolicitud`),
+  ADD UNIQUE KEY `nsolicitud` (`nsolicitud`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud_pedido`
+--
+ALTER TABLE `solicitud_pedido`
+  MODIFY `nsolicitud` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
