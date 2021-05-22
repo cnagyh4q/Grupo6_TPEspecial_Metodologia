8c3bab20be5ecfd25eef0d98cba3249af61865ee
ClaraCamejo <clara.camejo@hotmail.com>
2021-05-22 15:14:23 -0300

    arreglo model

--- CRU_TANDIL/Model/SolicitudesModel.php
+++ CRU_TANDIL/Model/SolicitudesModel.php
@@ -8,8 +8,9 @@ class solicitudesModel{
         $this->db = new PDO('mysql:host=localhost;'.'dbname=Cooperativa;charset=utf8','root','');
     }
 
-
-    function agregarSolicitud($nombre, $direccion, $telefono, $franja_horaria, $volumen_materiales){//tengo que tereminar este
+    /*Parametros: nombre->nombre_apellido, direccion, telefono, franja horaria, y volumen de materiales
+    Carga la informacion que recibe por paramtros y la guarda en la base de datos */
+    function agregarSolicitud($nombre, $direccion, $telefono, $franja_horaria, $volumen_materiales){
         
         $sentencia=$this->db->prepare('INSERT INTO solicitud_pedido (nombre_apellido, direccion, telefono, franja_horaria, volumen_materiales) VALUES (?,?,?,?,?)');
         $sentencia->execute([$nombre, $direccion, $telefono, $franja_horaria, $volumen_materiales]);
