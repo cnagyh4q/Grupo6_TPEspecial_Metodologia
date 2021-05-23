2f79bf9f6519bd382cf463016bbe5aa37a596291
ClaraCamejo <clara.camejo@hotmail.com>
2021-05-23 13:05:48 -0300

    GTM 20

---
+++ CRU_TANDIL/Model/MaterialModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class MaterialModel{
+
+    private $db;
+
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=Cooperativa;charset=utf8','root','');
+    }
+
+    /*Parametros: material, tratamiento
+    Carga la informacion que recibe por paramtros y la guarda en la base de datos */
+    function agregarMaterial($material, $tratamiento){
+        
+        $sentencia=$this->db->prepare('INSERT INTO material (tipo_material, tratamiento) VALUES (?,?)');
+        $sentencia->execute([$material, $tratamiento]);
+        
+        return $this->db->lastInsertId();
+    }
+
+}
