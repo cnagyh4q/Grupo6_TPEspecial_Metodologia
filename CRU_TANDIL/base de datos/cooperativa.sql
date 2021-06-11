4457002fc8c76456d89f0f214f8485bf91443d91
ClaraCamejo <clara.camejo@hotmail.com>
2021-06-10 22:33:15 -0300

    gtm-33 creacion de la tabla y modelo de pesajes

--- CRU_TANDIL/base de datos/cooperativa.sql
+++ CRU_TANDIL/base de datos/cooperativa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2021 a las 17:17:32
+-- Tiempo de generación: 11-06-2021 a las 03:25:48
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -34,6 +34,29 @@ CREATE TABLE `material` (
   `tratamiento` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `material`
+--
+
+INSERT INTO `material` (`nmaterial`, `tipo_material`, `tratamiento`) VALUES
+(2, 'Botellas de vidrio', 'Se deben entregar sanas y sin residuos de líquido en su interior'),
+(3, 'Latas de aluminio', 'Se deben entregar secas y aplastadas'),
+(4, 'Cajas de cartón', 'Se entregarán desarmadas y limpias');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pesaje_materiales`
+--
+
+CREATE TABLE `pesaje_materiales` (
+  `npesajemateriales` bigint(20) UNSIGNED NOT NULL,
+  `id` int(20) DEFAULT NULL,
+  `peso` double NOT NULL,
+  `material` varchar(200) NOT NULL,
+  `rol` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +73,13 @@ CREATE TABLE `solicitud_pedido` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Volcado de datos para la tabla `solicitud_pedido`
+--
+
+INSERT INTO `solicitud_pedido` (`nsolicitud`, `nombre_apellido`, `direccion`, `telefono`, `franja_horaria`, `volumen_materiales`) VALUES
+(1, 'Ernesto', 'Sarmiento 1387', '1549847383', '1', '2');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -59,6 +89,13 @@ CREATE TABLE `solicitud_pedido` (
 ALTER TABLE `material`
   ADD PRIMARY KEY (`nmaterial`),
   ADD UNIQUE KEY `nmaterial` (`nmaterial`);
+
+--
+-- Indices de la tabla `pesaje_materiales`
+--
+ALTER TABLE `pesaje_materiales`
+  ADD PRIMARY KEY (`npesajemateriales`),
+  ADD UNIQUE KEY `npesajemateriales` (`npesajemateriales`);
 
 --
 -- Indices de la tabla `solicitud_pedido`
@@ -75,13 +112,19 @@ ALTER TABLE `solicitud_pedido`
 -- AUTO_INCREMENT de la tabla `material`
 --
 ALTER TABLE `material`
-  MODIFY `nmaterial` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `nmaterial` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `pesaje_materiales`
+--
+ALTER TABLE `pesaje_materiales`
+  MODIFY `npesajemateriales` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `solicitud_pedido`
 --
 ALTER TABLE `solicitud_pedido`
-  MODIFY `nsolicitud` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `nsolicitud` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
