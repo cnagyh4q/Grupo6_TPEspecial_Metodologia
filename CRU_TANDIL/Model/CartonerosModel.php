04170538329737b4ee2190a909a78ba20a91876d
ClaraCamejo <clara.camejo@hotmail.com>
2021-07-03 22:40:44 -0300

    GTM-48 model

--- CRU_TANDIL/Model/CartonerosModel.php
+++ CRU_TANDIL/Model/CartonerosModel.php
@@ -8,13 +8,13 @@ class CartonerosModel{
         $this->db = new PDO('mysql:host=localhost;'.'dbname=Cooperativa;charset=utf8','root','');
     }
 
-    /*Parametros: nombre, vehiculo, dni, direccion, nacimiento
+    /*Parametros: nombre, vehiculo, dni, direccion, nacimiento, telefono
     Carga la informacion del xartonero que recibe y la guarda en la base de datos
-    direccion y nacimiento pueden ser nulos */
-    function agregarCartonero($nombre, $vehiculo, $dni, $direccion, $nacimiento){
+    telefono, direccion y nacimiento pueden ser nulos */
+    function agregarCartonero($nombre, $vehiculo, $dni, $direccion, $nacimiento, $telefono){
         
-        $sentencia=$this->db->prepare('INSERT INTO cartoneros (nombre, vehiculo, dni, direccion, nacimiento) VALUES (?,?,?,?,?)');
-        $sentencia->execute([$nombre, $vehiculo, $dni, $direccion, $nacimiento]);
+        $sentencia=$this->db->prepare('INSERT INTO cartoneros (nombre, vehiculo, dni, direccion, nacimiento, telefono) VALUES (?,?,?,?,?,?)');
+        $sentencia->execute([$nombre, $vehiculo, $dni, $direccion, $nacimiento, $telefono]);
         
         return $this->db->lastInsertId();
     }
